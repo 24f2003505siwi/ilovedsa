67e91303df1f948927580048f916391524ec3d5c
siri <24f2003505@ds.study.iitm.ac.in>
2026-09-16 13:29:21 +0530

    Time: 55 ms (21.17%) | Memory: 20.8 MB (62.10%) - LeetSync

---
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void dfs(int i, vector<vector<int>>& adj, vector<bool>& visited) {
+        visited[i] = true;
+        for (int neighbor : adj[i]) {
+            if (!visited[neighbor]) {
+                dfs(neighbor, adj, visited);
+            }
+        }
+    }
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+        vector<vector<int>> adj(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++){
+                if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]){
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
+            }
+        }
+        vector<bool> visited(n, false);
+        int components = 0;
+        for (int i = 0; i < stones.size(); i++) {
+            if (!visited[i]) {
+                components++;
+                dfs(i, adj, visited);
+            }
+        }
+        return n - components;
+    }
+};
